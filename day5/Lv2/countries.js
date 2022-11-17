e40534ba2cd20d9facdb5e00e189366d4419f61b
RrLSn <afolabisodiqtemitope@yahoo.com>
2022-11-17 20:02:15 +0100

    ty

---
+++ day5/Lv2/countries.js
@@ -0,0 +1,23 @@
+// 1. Create a separate countries.js file and store the countries array in to this file, create a separate file web_techs.js and store the webTechs array in to this file. Access both file in main.js file
+const countries = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ]
+
+  // 4. In countries array check if 'Ethiopia' exists in the array if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+// const country = countries.includes('Ethopia')
+// if (country){
+//   console.log(${countries}.toUpperCase())
+// }
+// else{
+//   console.log(countries.push('Ethopia'))
+// }
---
+++ day5/Lv2/main.js
@@ -0,0 +1,47 @@
+
+//   2. First remove all the punctuations and change the string to array and count the number of words in the array
+  let text =
+  'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+const words = text.split(',')
+console.log(words)
+console.log(words.length)
+
+//3.  In the following shopping cart add, remove, edit items
+   const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+
+//    add 'Meat' in the beginning of your shopping cart if it has not been already added
+shoppingCart.unshift('Meat')
+console.log(shoppingCart)
+
+// add Sugar at the end of you shopping cart if it has not been already added
+shoppingCart.push('Sugar')
+console.log(shoppingCart)
+
+// remove 'Honey' if you are allergic to honey
+// shoppingCart.pop(3)
+// console.log(shoppingCart)
+
+// modify Tea to 'Green Tea'
+shoppingCart[3] = 'Green Tea'
+console.log(shoppingCart)
+
+//6. Concatenate the following two variables and store it in a fullStack variable.
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+const fullStack = frontEnd.concat(backEnd)
+console.log(fullStack)
+
+
+// Exercise Lv3
+
+//1. The following is an array of 10 students ages:
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+// Sort the array and find the min and max age
+console.log(ages.sort())
+const age = ages.mingit
+console.log(age)
+// Find the median age(one middle item or two middle items divided by two)
+// Find the average age(all items divided by number of items)
+// Find the range of the ages(max minus min)
+// Compare the value of (min - average) and (max - average), use abs() method 1.Slice the first ten countries from the countries array
+
