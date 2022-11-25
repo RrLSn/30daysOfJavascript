3743a974319173516f76a4dc20f9cebbc623b65c
RrLSn <afolabisodiqtemitope@yahoo.com>
2022-11-25 07:24:16 +0100

    30

---
+++ day8/Lv1/main.js
@@ -0,0 +1,17 @@
+// 1. Create an empty object called dog
+const dog = {}
+
+// 2. Print the the dog object on the console
+console.log(dog)
+
+// 3. Add name, legs, color, age and bark properties for the dog object. The bark property is a method which return woof woof
+dog.name = 'Eskimo'
+dog.color = 'White'
+dog.legs = '4legs'
+dog.age = '4months'
+
+// 4. Get name, legs, color, age and bark value from the dog object
+console.log(dog)
+
+// 5. Set new properties the dog object: breed, getDogInfo
+dog.object = ['breed', 'getDogInfo']
