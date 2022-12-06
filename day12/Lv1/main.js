60e6955a3c60cb69cb07281e20519657654331c6
RrLSn <afolabisodiqtemitope@yahoo.com>
2022-12-06 15:51:55 +0100

    30

--- day12/Lv1/main.js
+++ day12/Lv1/main.js
@@ -6,3 +6,29 @@ console.log(Number(addition[0])+Number(addition[1])+Number(addition[2]))
 
 
 // 2. The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers and find the distance between the two furthest particles.
+
+const words = 'The position of some particles on the horizontal x-axis -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction.'
+const patterN = /-?[0-9]/g
+const points = words.match(patterN)
+console.log(points)
+
+const sortedPoints = points.sort((a,b) => a-b)
+console.log(sortedPoints)
+
+
+// 3. Write a pattern which identify if a string is a valid JavaScript variable
+const isValid = (variable) => {
+    const variable = 'Afolabi'
+    if(variable.includes('_')){
+        return true
+    } else if(variable.includes('-')){
+        return false
+    } else if(variable.startsWith('number')){
+        return true
+    }else{
+        return true
+    }
+}
+
+isValid()
+
