fb5a9f016e46eec6b1b1feda4b251866b9cca4d2
RrLSn <afolabisodiqtemitope@yahoo.com>
2022-12-16 02:04:19 +0100

    40

---
+++ day18/main.js
@@ -0,0 +1,10 @@
+// Exercise 1
+const countriesAPI = 'https://restcountries.com/v2/all'
+const catsAPI = 'https://api.thecatapi.com/v1/breeds'
+
+// 1. Read the countries API using fetch and print the name of country, capital, languages, population and area.
+fetch(countriesAPI)
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+})
