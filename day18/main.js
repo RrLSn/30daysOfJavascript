043d659afd377c027504a94012a53492a353d126
RrLSn <afolabisodiqtemitope@yahoo.com>
2022-12-17 06:00:44 +0100

    60

--- day18/main.js
+++ day18/main.js
@@ -11,6 +11,22 @@ fetch(countriesAPI)
     
 })
 
+const FetchCountInfo = async () => {
+    try {
+        let arr = []
+        const response = await fetch(countriesAPI)
+        const result = await response.json()
+        result.filter((ans) => {
+            arr.push(`country: ${ans.name}, capital: ${ans.capital}, population: ${ans.population}, languages: ${ans.languages[1]}, area: ${ans.area}`)
+        })
+        console.log(arr)
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+FetchCountInfo()
 
 // Exercise 2
 // Print out all the cat names in to catNames variable.
@@ -20,4 +36,23 @@ fetch(catsAPI)
     let answer = info.map(i => i.name)
     console.log(answer)
 })
+
+const fetchCatInfo = async () => {
+    try {
+        let emptyArr = []
+        const response = await fetch(catsAPI)
+        const result = await response.json()
+        result.filter((res) => {
+            emptyArr.push(`${res.name}`)
+        })
+        console.log(emptyArr)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+fetchCatInfo()
+
+// Exercise 3
+// 1. Read the cats api and find the average weight of cat in metric unit.
 
