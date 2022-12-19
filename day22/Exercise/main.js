c95410d2d2e2e067abd67dfce22fdbce62adbcd7
RrLSn <afolabisodiqtemitope@yahoo.com>
2022-12-19 17:24:12 +0100

    70

--- day22/Exercise/main.js
+++ day22/Exercise/main.js
@@ -1,11 +1,7 @@
 const bodyEl = document.querySelector('body')
-// bodyEl.style.textAlign ='center'
 bodyEl.style.fontFamily = 'segoe UI'
 bodyEl.style.width = '100vw'
-// bodyEl.style.height = '100vh'
-bodyEl.style.overflow = 'hidden'
 bodyEl.style.cursor = 'pointer'
-
 
 const h1El = document.createElement('h1')
 h1El.textContent = 'Number Generator'
@@ -17,7 +13,7 @@ bodyEl.appendChild(h1El)
 const h2El = document.createElement('h2')
 h2El.textContent = '30DaysOfJavaScript:DOM Day 2'
 h2El.style.textDecoration = 'underline'
-h2El.style.fontWeight = '500'
+h2El.style.fontWeight = '400'
 h2El.style.width = 'max-content'
 h2El.style.margin = 'auto'
 bodyEl.appendChild(h2El)
@@ -26,13 +22,52 @@ const h3El =  document.createElement('h3')
 h3El.textContent = 'Author: Afolabi Sodiq'
 h3El.style.textDecoration = 'underline'
 h3El.style.margin = 'auto'
+h3El.style.fontWeight = '400'
 h3El.style.width = 'max-content'
 bodyEl.appendChild(h3El)
 
 const divEl = document.createElement('div')
+divEl.style.width = '50%'
+divEl.style.height = '130vh'
+divEl.style.margin = 'auto'
+divEl.style.display = 'grid'
+divEl.style.gridTemplateColumns = 'repeat(6,1fr)'
+divEl.style.gridGap = '0.4rem'
+
+
+function isPrime(n) {
+    if (n < 2) {
+      return false;
+    }
+    if (n == 2) {
+      return true;
+    }
+    for (let i = 2; i < n; i++) {
+      if (n % i == 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+for(let i=0; i<=101; i++){
+    const pEl = document.createElement('p')
+    pEl.textContent = i
+    divEl.appendChild(pEl)
+    pEl.style.width = '100%'   
+    pEl.style.height = '100%'
+    pEl.style.color = 'white'
+    pEl.style.display = 'flex'
+    pEl.style.fontSize = '2rem'
+    pEl.style.justifyContent = 'center'
+    pEl.style.alignItems = 'center'
+    if(i%2 === 0){
+        pEl.style.background = 'green'
+    }else if(isPrime(i)){
+        pEl.style.background = 'red'
+    } else{
+        pEl.style.background = 'yellow'
+    }
+}
 bodyEl.appendChild(divEl)
-// for(let i=0; 1<100; i++){
-//     const h1El = document.createElement('h1')
-//     h1El.textContent = `${i + 1}`
-//     divEl.appendChild(h1El)
-// }
+
