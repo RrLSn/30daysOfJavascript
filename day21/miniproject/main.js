dcb7a2f0bcde53d0c09d5e2110b78069067e7c68
RrLSn <afolabisodiqtemitope@yahoo.com>
2022-12-19 15:27:52 +0100

    5

--- day21/miniproject/main.js
+++ day21/miniproject/main.js
@@ -1,6 +1,7 @@
 bodyEl = document.querySelector('body');
 bodyEl.style.width = '100vw';
-bodyEl.style.fontfamily = 'segoe UI';
+bodyEl.style.fontFamily = 'segoe UI';
+bodyEl.style.cursor = 'pointer'
 
 wrappyEl = document.querySelector('.wrapper');
 wrappyEl.style.width = '50%';
@@ -8,6 +9,7 @@ wrappyEl.style.margin = 'auto';
 
 h1El = document.querySelector('h1');
 h1El.style.textAlign = 'center';
+h1El.style.fontWeight = '500'
 
 h2El = document.querySelector('h2');
 h2El.style.textAlign = 'center';
@@ -16,7 +18,12 @@ h2El.style.fontWeight = '100';
 
 
 spanEl = document.querySelector('span');
-spanEl.style.fontSize = '4rem';
+spanEl.style.fontSize = '5rem';
+spanEl.style.fontWeight = 'bold'
+const rdcolor = () => {
+    spanEl.style.color = '#'+ Math.random().toString(16).substr(2,6)
+}
+setInterval(rdcolor,1000)
 
 const monthNames = [
   "January",
@@ -46,9 +53,26 @@ let sec = second < 10 ? `0${second}`: `${second}`
 
 const date = `${monthNames[month]} ${day}, ${year} ${hr}:${min}:${sec}`
 
-wrappyEl.innerHTML = `<div>${date}</div>`;
-// divEl = document.querySelector('div')
-// divEl.style.background = 'red'
-// divEl.style.textAlign = 'center'
-// divEl.style.width = '25%'
-// divEl.style.height = '3rem'
+const asideEl = document.querySelector('aside');
+asideEl.innerHTML = date;
+asideEl.style.textAlign = 'center';
+asideEl.style.width =  'max-content';
+asideEl.style.margin = 'auto';
+asideEl.style.padding = '0.5rem 1.5rem';
+asideEl.style.fontSize = '0.9rem';
+asideEl.style.fontWeight = 'bold';
+const rdbackground = () => {
+    asideEl.style.background = '#'+ Math.random().toString(16).substr(2,6);
+}
+setInterval(rdbackground,1000)
+
+const lisEl = document.querySelectorAll('li');
+
+for(let i=0; i<lisEl.length; i++){
+    lisEl[i].style.background = '#eb695b';
+    lisEl[i].style.listStyle = 'none';
+    lisEl[i].style.marginBottom = '0.2rem';
+    lisEl[i].style.padding = '1rem';
+    lisEl[0].style.background = '#5bbc7a';
+    lisEl[1].style.background = '#f7dc5c';
+}
