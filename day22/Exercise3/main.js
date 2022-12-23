3f2f571cf6c05ce240c9dc0a243a7c6b5ca39992
RrLSn <afolabisodiqtemitope@yahoo.com>
2022-12-24 00:34:41 +0100

    4

--- day22/Exercise3/main.js
+++ day22/Exercise3/main.js
@@ -1,201 +1,202 @@
 const asabenehChallenges2020 = {
-    description: 'Asabeneh Yetayeh challenges',
-    challengeTitle: 'Asabeneh Yetayeh challenges in ',
-    challengeSubtitle: '30DaysOfJavaScript Challenge',
-    challengeYear: 2020,
-    keywords: [
-      'HTML',
-      'HTML5',
-      'CSS',
-      'CSS3',
-      'JS',
-      'JavaScript',
-      'ES6',
-      'Promise',
-      'async await',
-      'Database',
-      'React',
-      'React Hooks',
-      'Context API',
-      'React Router',
-      'Web Storage',
-      'localStorage',
-      'sessionStorage',
-      'Redux',
-      'Node',
-      'MongoDB',
-      'SQL',
-      'API',
-      'DOM',
-      'data science',
-      'MERN',
-      'Python',
-      'Flask',
-      'Statistics',
-      'Linear Algebra',
-      'Numpy',
-      'Pandas',
-      'Scipy',
-      'Scikit-learn',
-      'Visualization',
-      'D3.js'
+  description: "Asabeneh Yetayeh challenges in",
+  challengeTitle: "Asabeneh Yetayeh",
+  challengeSubtitle: "30DaysOfJavaScript Challenge",
+  challengeYear: 2020,
+  keywords: [
+    "HTML",
+    "HTML5",
+    "CSS",
+    "CSS3",
+    "JS",
+    "JavaScript",
+    "ES6",
+    "Promise",
+    "async await",
+    "Database",
+    "React",
+    "React Hooks",
+    "Context API",
+    "React Router",
+    "Web Storage",
+    "localStorage",
+    "sessionStorage",
+    "Redux",
+    "Node",
+    "MongoDB",
+    "SQL",
+    "API",
+    "DOM",
+    "data science",
+    "MERN",
+    "Python",
+    "Flask",
+    "Statistics",
+    "Linear Algebra",
+    "Numpy",
+    "Pandas",
+    "Scipy",
+    "Scikit-learn",
+    "Visualization",
+    "D3.js",
+  ],
+  author: {
+    firstName: "Asabeneh",
+    lastName: "Yetayeh",
+    titles: [
+      ["🌱", "Educator"],
+      ["💻", "Programmer"],
+      ["🌐", "Developer"],
+      ["🔥", "Motivator"],
+      ["📔", "Content Creator"],
     ],
-    author: {
-      firstName: 'Asabeneh',
-      lastName: 'Yetayeh',
-      titles: [
-        ['🌱', 'Educator'],
-        ['💻', 'Programmer'],
-        ['🌐', 'Developer'],
-        ['🔥', 'Motivator'],
-        ['📔', 'Content Creator']
+    qualifications: [
+      "📖 MSc. Computer Science Ongoing",
+      "👨🏾‍🎓 BSc. Information and Communication Eng.",
+      "👩🏾‍🎓 MSc. Food Technology",
+      "👨🏾‍🎓 BSc.Food Technology",
+    ],
+    socialLinks: [
+      {
+        social: "LinkedIn",
+        url: "https://www.linkedin.com/in/asabeneh/",
+        fontawesomeIcon: '<i class="fab fa-linkedin">',
+      },
+      {
+        social: "Twitter",
+        url: "https://twitter.com/Asabeneh",
+        fontawesomeIcon: '<i class="fab fa-twitter-square"></i>',
+      },
+      {
+        social: "Github",
+        fontawesomeIcon: '<i class="fab fa-github-square"></i>',
+        url: "https://github.com/Asabeneh",
+      },
+      {
+        social: "DEV.to",
+        fontawesomeIcon: "",
+        url: "https://dev.to/asabeneh",
+      },
+    ],
+    skills: [
+      "✅ Web Development",
+      "✅ Data Analysis",
+      "✅ Data Visualization",
+      "✅ Programming",
+      "✅ Databases",
+      "✅ Developing API",
+    ],
+    bio: "I am an educator, developer, motivator and content creator. I am a life-long learner. If you like to know more about me checkout my LinkedIn or Github profile. Thank you so much for joining in my quest of changing everyone to developer.",
+  },
+  challenges: [
+    {
+      name: "30 Days Of Python",
+      topics: [
+        "Python",
+        "Flask",
+        "Numpy",
+        "Pandas",
+        "Statistics",
+        "API",
+        "MongoDB",
       ],
-      qualifications: [
-        'MSc. Computer Science Ongoing',
-        'BSc. Information and Communication Eng.',
-        'MSc. Food Technology',
-        'BSc.Food Technology'
-      ],
-      socialLinks: [
-        {
-          social: 'LinkedIn',
-          url: 'https://www.linkedin.com/in/asabeneh/',
-          fontawesomeIcon: '<i class="fab fa-linkedin">'
-        },
-        {
-          social: 'Twitter',
-          url: 'https://twitter.com/Asabeneh',
-          fontawesomeIcon: '<i class="fab fa-twitter-square"></i>'
-        },
-        {
-          social: 'Github',
-          fontawesomeIcon: '<i class="fab fa-github-square"></i>',
-          url: 'https://github.com/Asabeneh'
-        },
-        {
-          social: 'DEV.to',
-          fontawesomeIcon: '',
-          url: 'https://dev.to/asabeneh'
-        }
-      ],
-      skills: [
-        'Web Development',
-        'Data Analysis',
-        'Data Visualization',
-        'Programming',
-        'Databases',
-        'Developing API'
-      ],
-      bio:
-        'I am an educator, developer, motivator and content creator. I am a life-long learner. If you like to know more about me checkout my LinkedIn or Github profile. Thank you so much for joining in my quest of changing everyone to developer.'
+      days: 30,
+      status: "Done",
+      questions: "Above 500",
+      projects: "Two",
+      interviewQns: "",
+      githubUrl: "https://github.com/Asabeneh/30-Days-Of-Python",
     },
-    challenges: [
-      {
-        name: '30 Days Of Python',
-        topics: [
-          'Python',
-          'Flask',
-          'Numpy',
-          'Pandas',
-          'Statistics',
-          'API',
-          'MongoDB'
-        ],
-        days: 30,
-        status: 'Done',
-        questions: 'Above 500',
-        projects: 'Two',
-        interviewQns: '',
-        githubUrl: 'https://github.com/Asabeneh/30-Days-Of-Python'
-      },
-      {
-        name: '30 Days Of JavaScript',
-        topics: ['JavaScript', 'ES6', 'Promise', 'async and await', 'DOM'],
-        days: 30,
-        status: 'Ongoing',
-        questions: 'Above 500',
-        projects: 'About 30',
-        interviewQns: '',
-        githubUrl: 'https://github.com/Asabeneh/30DaysOfJavaScript'
-      },
-      {
-        name: '30 Days Of HTML & CSS',
-        topics: ['CSS', 'Flex', 'Grid', 'CSS Animation'],
-        days: 30,
-        status: 'Coming',
-        questions: 'Above 500',
-        projects: 'Two',
-        interviewQns: '',
-        githubUrl: ''
-      },
-      {
-        name: '30 Days Of React',
-        topics: [
-          'React',
-          'React Router',
-          'Redux',
-          'Context API',
-          'React Hooks',
-          'MERN'
-        ],
-        days: 30,
-        status: 'Coming',
-        questions: '',
-        projects: '',
-        interviewQns: '',
-        githubUrl: ''
-      },
-      {
-        name: '30 Days Of ReactNative',
-        topics: ['ReactNative', 'Redux'],
-        days: 30,
-        status: 'Coming',
-        questions: '',
-        projects: 'Two',
-        interviewQns: '',
-        githubUrl: ''
-      },
-      {
-        name: '30 Days Of Fullstack',
-        topics: ['React', 'Redux', 'MongoDB', 'Node', 'MERN'],
-        days: 30,
-        status: 'Coming',
-        questions: '',
-        projects: '',
-        interviewQns: '',
-        githubUrl: ''
-      },
-      {
-        name: '30 Days Of Data Analysis',
-        topics: ['Python', 'Numpy', 'Pandas', 'Statistics', 'Visualization'],
-        days: 30,
-        status: 'Coming',
-        questions: '',
-        projects: '',
-        interviewQns: '',
-        githubUrl: ''
-      },
-      {
-        name: '30 Days Of Machine Learning',
-        topics: [
-          'Python',
-          'Numpy',
-          'Pandas',
-          'Scikit-learn',
-          'Scipy',
-          'Linear Algebra',
-          'Statistics',
-          'Visualization'
-        ],
-        days: 30,
-        status: 'Coming',
-        questions: '',
-        projects: '',
-        interviewQns: '',
-        githubUrl: ''
-      }
-    ]
-  }
+    {
+      name: "30 Days Of JavaScript",
+      topics: ["JavaScript", "ES6", "Promise", "async and await", "DOM"],
+      days: 30,
+      status: "Ongoing",
+      questions: "Above 500",
+      projects: "About 30",
+      interviewQns: "",
+      githubUrl: "https://github.com/Asabeneh/30DaysOfJavaScript",
+    },
+    {
+      name: "30 Days Of HTML & CSS",
+      topics: ["CSS", "Flex", "Grid", "CSS Animation"],
+      days: 30,
+      status: "Coming",
+      questions: "Above 500",
+      projects: "Two",
+      interviewQns: "",
+      githubUrl: "",
+    },
+    {
+      name: "30 Days Of React",
+      topics: [
+        "React",
+        "React Router",
+        "Redux",
+        "Context API",
+        "React Hooks",
+        "MERN",
+      ],
+      days: 30,
+      status: "Coming",
+      questions: "",
+      projects: "",
+      interviewQns: "",
+      githubUrl: "",
+    },
+    {
+      name: "30 Days Of ReactNative",
+      topics: ["ReactNative", "Redux"],
+      days: 30,
+      status: "Coming",
+      questions: "",
+      projects: "Two",
+      interviewQns: "",
+      githubUrl: "",
+    },
+    {
+      name: "30 Days Of Fullstack",
+      topics: ["React", "Redux", "MongoDB", "Node", "MERN"],
+      days: 30,
+      status: "Coming",
+      questions: "",
+      projects: "",
+      interviewQns: "",
+      githubUrl: "",
+    },
+    {
+      name: "30 Days Of Data Analysis",
+      topics: ["Python", "Numpy", "Pandas", "Statistics", "Visualization"],
+      days: 30,
+      status: "Coming",
+      questions: "",
+      projects: "",
+      interviewQns: "",
+      githubUrl: "",
+    },
+    {
+      name: "30 Days Of Machine Learning",
+      topics: [
+        "Python",
+        "Numpy",
+        "Pandas",
+        "Scikit-learn",
+        "Scipy",
+        "Linear Algebra",
+        "Statistics",
+        "Visualization",
+      ],
+      days: 30,
+      status: "Coming",
+      questions: "",
+      projects: "",
+      interviewQns: "",
+      githubUrl: "",
+    },
+  ],
+};
+
+
 
 bodyEl = document.querySelector('body')
 bodyEl.style.widows = '100vw'
@@ -618,6 +619,18 @@ h3El.style.fontWeight = 'bold'
 h3El.style.fontSize = '2rem'
 mainEl.appendChild(h3El)
 
+const social = document.createElement('div')
+social.style.display = 'flex'
+social.style.justifyContent = 'space-between'
+social.style.width = '30%'
+social.style.margin = '0 auto 1rem'
+for(i=0; i<3; i++){
+const iconSocio = document.createElement('span')
+iconSocio.innerHTML = asabenehChallenges2020.author.socialLinks[i].social
+social.appendChild(iconSocio)
+}
+mainEl.appendChild(social)
+
 const h1El2 = document.createElement('h1')
 h1El2.textContent = asabenehChallenges2020.author.bio
 h1El2.style.fontSize = '1rem'
@@ -627,6 +640,8 @@ h1El2.style.margin = 'auto'
 h1El2.style.textAlign = 'center'
 mainEl.appendChild(h1El2)
 
+
+// for title 
 const newDivEl = document.createElement('div')
 newDivEl.style.display = 'grid'
 newDivEl.style.gridTemplateColumns = '1fr 1fr 1fr'
@@ -639,24 +654,38 @@ blkaH1.textContent = 'Titles'
 blkaH1.style.fontSize = '1.5rem'
 
 const blkapEl = document.createElement('p')
-blkapEl.textContent = asabenehChallenges2020.author.titles
+for(i=0; i<asabenehChallenges2020.author.titles.length; i++){
+const blkapEl2 = document.createElement('p')
+blkapEl2.textContent += asabenehChallenges2020.author.titles[i][0] +
+"" 
++asabenehChallenges2020.author.titles[i][1]
+blkapEl2.style.lineHeight = '0.8rem'
+blkapEl.appendChild(blkapEl2)
+}
 
 blkaEl.appendChild(blkaH1)
 blkaEl.appendChild(blkapEl)
 newDivEl.appendChild(blkaEl)
 
+// for skills
 const blkbEl = document.createElement('div')
 const blkbH1 = document.createElement('h1')
 blkbH1.textContent = 'skills'
 blkbH1.style.fontSize = '1.5rem'
 
 const blkbpEl = document.createElement('p')
-blkbpEl.textContent = asabenehChallenges2020.author.skills
+for(i=0; i<asabenehChallenges2020.author.skills.length; i++){
+const blkbpEl2 = document.createElement('p')
+blkbpEl2.textContent += asabenehChallenges2020.author.skills[i]
+blkbpEl2.style.lineHeight = '0.8rem'
+blkbpEl.appendChild(blkbpEl2)
+}
 
 blkbEl.appendChild(blkbH1)
 blkbEl.appendChild(blkbpEl)
 newDivEl.appendChild(blkbEl)
 
+// for Qualifications
 const blkcEl = document.createElement('div')
 blkcEl.style.direction ='block'
 const blkcH1 = document.createElement('h1')
@@ -664,7 +693,13 @@ blkcH1.textContent = 'Qualifications'
 blkcH1.style.fontSize = '1.5rem'
 
 const blkcpEl = document.createElement('p')
-blkcpEl.textContent = asabenehChallenges2020.author.qualifications
+for(i=0; i<asabenehChallenges2020.author.qualifications.length; i++){
+const blkcpEl2 = document.createElement('p')
+blkcpEl2.textContent += asabenehChallenges2020.author.qualifications[i]
+blkcpEl2.style.lineHeight = '0.8rem'
+blkcpEl.appendChild(blkcpEl2)
+}
+
 
 blkcEl.appendChild(blkcH1)
 blkcEl.appendChild(blkcpEl)
@@ -675,11 +710,22 @@ mainEl.appendChild(newDivEl)
 const keyEl = document.createElement('h1')
 keyEl.textContent = 'Keywords'
 keyEl.style.fontSize = '1.5rem'
-keyEl.style.margin = '0 2rem'
+keyEl.style.margin = '2rem'
 
-const keyPEl = document.createElement('p')
+const keyPEl = document.createElement('div')
+keyPEl.style.display = 'flex'
+keyPEl.style.flexWrap = 'wrap'
+keyPEl.style.justifyContent = 'space-between'
 for(i=0; i<asabenehChallenges2020.keywords.length; i++){
-  keyPEl.textContent = keywords[i]
+const keyPEl2 = document.createElement('p')
+keyPEl2.textContent = `#${asabenehChallenges2020.keywords[i]}`
+keyPEl2.style.width = 'max-content'
+keyPEl2.style.padding = '0.5rem 2rem 0.5rem 1rem'
+keyPEl2.style.fontSize = '1rem'
+keyPEl2.style.marginBottom = '0'
+keyPEl2.style.borderRadius = '15px'
+keyPEl2.style.background = '#' + Math.floor(Math.random()*16777215).toString(16)
+keyPEl.appendChild(keyPEl2)
 }
 keyEl.appendChild(keyPEl)
 
